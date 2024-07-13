b1a599d99bb0bdd745aa1b7e1d05e822c42b6bb5
PavaniSurisetty <pavsurisetty@gmail.com>
2024-07-13 11:49:07 +0530

    adding encrypt.sh

--- encrypt.sh
+++ encrypt.sh
@@ -3,14 +3,14 @@ bash
 #!/bin/bash
 
 # Encrypt the word "land" using openssl
-encrypted=$(echo -n "land" | openssl enc -aes-256-cbc -a -salt -pbkdf2 -iter 10000 -pass pass:your_password)
+encrypted=$(echo -n "land" | openssl enc -aes-256-cbc -a -salt -pbkdf2 -iter 10000 -pass pass:myname)
 
 # Print the encrypted word
 echo $encrypted
 
 
 # Decrypt the encrypted word
-decrypted=$(echo -n "$encrypted" | openssl enc -aes-256-cbc -a -salt -pbkdf2 -iter 10000 -pass pass:your_password -d)
+decrypted=$(echo -n "$encrypted" | openssl enc -aes-256-cbc -a -salt -pbkdf2 -iter 10000 -pass pass:myname -d)
 
 # Print the decrypted word
 echo $decrypted
